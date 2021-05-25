d3fad439a2664941c2a12350ca31d1681c3fb0b2
Enzo Aramayo <enzosandoval@hotmail.com>
2021-05-25 20:33:21 -0300

    Base

---
+++ repo-grupo-fernet-tp7/src/main/java/ar/edu/unju/fi/tp7/RepoGrupoFernetTp7Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoGrupoFernetTp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoGrupoFernetTp7Application.class, args);
+	}
+
+}
---
+++ repo-grupo-fernet-tp7/src/test/java/ar/edu/unju/fi/tp7/RepoGrupoFernetTp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoGrupoFernetTp7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
